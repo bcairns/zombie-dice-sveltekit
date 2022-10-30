2c3ebca3e00d68c13143ee7a8a5d3b0ce68717d0
Brian Cairns <bcairns@gmail.com>
2022-10-30 12:02:48 -0600

    dope ass title gradient

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -44,6 +44,13 @@
         font-size: 5em;
         color: #EA1F27;
     }
+    @supports (-webkit-background-clip: text) {
+        h1 {
+            background-image: linear-gradient(to top, #400, #E21143 50%, #FFF03A 100%);
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+        }
+    }
     main {
         display: grid;
         gap: 1em;
